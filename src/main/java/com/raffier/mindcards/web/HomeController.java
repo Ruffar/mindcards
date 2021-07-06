10c75cbb33f239433057af75219e145994073d10
Ruffar <raffier.utomo@gmail.com>
2021-07-06 10:38:48 +0100

    got controller to work

---
+++ src/main/java/com/raffier/mindcards/web/HomeController.java
@@ -0,0 +1,16 @@
+package com.raffier.mindcards.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+    
+}
