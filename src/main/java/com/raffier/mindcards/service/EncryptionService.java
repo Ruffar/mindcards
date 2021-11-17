89b669d1796a3a05e0ead8e922d43142107d47f1
Ruffar <raffier.utomo@gmail.com>
2021-11-17 22:18:42 +0000

    salt random

--- src/main/java/com/raffier/mindcards/service/EncryptionService.java
+++ src/main/java/com/raffier/mindcards/service/EncryptionService.java
@@ -11,8 +11,8 @@ import javax.crypto.spec.SecretKeySpec;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
-import java.security.spec.RSAPrivateKeySpec;
 import java.util.Base64;
+import java.util.Random;
 
 @Service
 public class EncryptionService {
@@ -27,14 +27,8 @@ public class EncryptionService {
     public String encryptPassword(String plainPassword, User user) {
         try {
 
-            byte[] userIdBytes = Integer.toBinaryString(user.getUserId()).getBytes();
-            //Create a salt of 16 bytes using userId
             byte[] salt = new byte[16];
-            for (int i = 0; i < 16; i++) {
-                if (userIdBytes.length-1 <= i) { //If the byte from userId exists then add it to the salt
-                    salt[i] = userIdBytes[i];
-                }
-            }
+            new Random(user.getUserId()).nextBytes(salt);
 
             KeySpec keySpec = new PBEKeySpec(plainPassword.toCharArray(), salt, 2^16, 256);
             SecretKey key = new SecretKeySpec(keyFactory.generateSecret(keySpec).getEncoded(), "AES");
